69514152958e9aa765059e53651262ec06029e46
Julian Gruber <julian@juliangruber.com>
2018-05-15 11:17:23 +0200

    standard

--- example.js
+++ example.js
@@ -2,7 +2,11 @@ const Screenshot = require('.')
 const fs = require('fs')
 const { join } = require('path')
 
-new Screenshot('http://ghub.io/').width(800).height(600).capture().then(img => {
-  fs.writeFileSync(join(__dirname, '/example.png'), img)
-  console.log('open example.png')
-})
+new Screenshot('http://ghub.io/')
+  .width(800)
+  .height(600)
+  .capture()
+  .then(img => {
+    fs.writeFileSync(join(__dirname, '/example.png'), img)
+    console.log('open example.png')
+  })
